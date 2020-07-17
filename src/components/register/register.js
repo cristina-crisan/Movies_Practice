0aff22096eecc87354799061b65961b9b7e5a670
Cristina CRISAN <cristina.crisan91@hotmail.com>
2020-07-17 12:36:48 +0300

    Register user

--- src/components/register/register.js
+++ src/components/register/register.js
@@ -2,9 +2,26 @@
 
 function register() {
   let username = document.querySelector(".user").value;
+  let userElement = document.querySelector(".user");
   let password = document.querySelector(".password").value;
+  let passwordElement = document.querySelector(".password");
 
-  HttpService.register(username, password).then(response => console.log(response))
+  if ((username.length < 8) || (password.length < 8)) {
+    userElement.style.borderBottomColor = "red";
+    userElement.value = "Username must have at least 8 characters.";
+    passwordElement.type = "text";
+    passwordElement.style.borderBottomColor = "red";
+    passwordElement.value = "Password must have at least 8 characters.";
+  }
+  if (username.length >= 8 && password.length >= 8) {
+    HttpService.register(username, password).then(response => {
+      console.log(response)
+    if (response.authenticated) {
+      localStorage.setItem("accessToken", response.accessToken);
+      localStorage.setItem("user", username);
+    }
+  });
+  }
 }
 
 
